b576c048356077d2fc4364c7e4f432ba9588b811
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-04-08 21:43:09 +0330

    feat: implement prevPage as header of pages

---
+++ src/components/prevPage/prevPage.js
@@ -0,0 +1,25 @@
+import { El } from "../../utils/shared/El";
+
+export function PrevPage(title="") {
+  return El({
+    element: "div",
+    className: "flex items-center py-3",
+    children: [
+      El({
+        element: "div",
+        children: [
+          El({
+            element: "img",
+            className: "",
+            src: "./src/assets/icons/arrow-left-short.svg",
+          }),
+        ],
+      }),
+      El({
+        element: "p",
+        className: "text-xl pl-3   font-semibold w-fit h-8 text-tangaroa flex items-center",
+        innerText: title,
+      }),
+    ],
+  });
+}
