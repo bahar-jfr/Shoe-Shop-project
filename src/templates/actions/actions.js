dcc354fe5ca7a5cf7ec89551e87da079618e7d59
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-04-11 19:31:11 +0330

    style: fix footer's position while scrolling page

--- src/templates/actions/actions.js
+++ src/templates/actions/actions.js
@@ -1,16 +1,16 @@
-import { ActionIcon } from "../../components/actionIcon/actionIcon";
+import { ActionIcon } from "../../components/HOME/actionIcon/actionIcon";
 import { El } from "../../utils/shared/El";
 
-export function actions(){
-    return El({
-        element: "div",
-        className: "flex items-center justify-between px-6",
-        children:[
-            ActionIcon("Home"),
-            ActionIcon("Cart"),
-            ActionIcon("Orders"),
-            ActionIcon("Wallet"),
-            ActionIcon("Profile")
-        ]
-    })
+export function actions() {
+  return El({
+    element: "div",
+    className: "flex items-center justify-between  fixed bottom-0 left-1/2 -translate-x-1/2 w-full px-12 py-3 bg-white",
+    children: [
+      ActionIcon("Home"),
+      ActionIcon("Cart"),
+      ActionIcon("Orders"),
+      ActionIcon("Wallet"),
+      ActionIcon("Profile"),
+    ],
+  });
 }
