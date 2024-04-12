0e59b93c1fa9aefb14c6abab978cf292b71b34e5
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-04-12 17:43:20 +0330

    fix: declare footer in pages if needed

--- src/layouts/layout.js
+++ src/layouts/layout.js
@@ -3,10 +3,11 @@ import { Footer } from "./footer/footer";
 import { Header } from "./header/header";
 import { Main } from "./main/main";
 
-export function Layout(header, main, footer,style) {
+export function Layout(header, main, isRenderFooter , footer, style) {
+  const renderFooter = !!isRenderFooter ? Footer(footer) : "";
   return El({
     element: "div",
-    className : `${style}`,
-    children: [Header(header), Main(main), Footer(footer)],
+    className: `${style}`,
+    children: [Header(header), Main(main), renderFooter],
   });
 }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,4 +1,3 @@
-import { BrandLogo } from "../../components/HOME/brandLogo/brandLogo";
 import { Layout } from "../../layouts/layout";
 import { actions } from "../../templates/actions/actions";
 import { homeSections } from "../../templates/homeSections/homeSections";
@@ -6,5 +5,5 @@ import { userInfo } from "../../templates/userInfo/userInfo";
 
 export function home() {
   const style = "flex flex-col px-6 pt-4 relative";
-  return Layout(userInfo(), homeSections(), actions(), style);
+  return Layout(userInfo(), homeSections(),true, actions(), style);
 }
