f7bac487e2f26af7df50fd17b5f6842a0bda42e7
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-04-12 17:40:07 +0330

    feat: declare the routes and navigate to them

---
+++ src/routes/path.js
@@ -0,0 +1,6 @@
+export const Main_Route = "/";
+export const Login_Route = "/login";
+export const Home_Route = "/home";
+export const Whish_Route = "/whishlist";
+export const Cart_Route = "/cart";
+export const Singel_Route = "/singelproduct"
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,9 @@
-import { PrevPage } from "../components/prevPage/prevPage";
 import Navigo from "navigo";
-import { login } from "../pages/login";
+import { login } from "../pages/login/login";
+import { home } from "../pages/home/home";
+import { Cart_Route, Home_Route, Login_Route, Main_Route, Singel_Route } from "./path";
+import { brandProducts } from "../pages/home/brandProducts";
+import { singelProduct } from "../pages/home/singelProduct";
 
 export let router = new Navigo("/");
 
@@ -10,10 +13,26 @@ const changePage = (page) => {
   app.appendChild(page);
 };
 
-export const Router = () => {
-  router.on("/", () => {
-    changePage(login());
-  });
+export const Router = (route) => {
+  router
+    .on(Main_Route, () => {
+      changePage(home());
+    })
+    .on(Login_Route, () => {
+      changePage(login());
+    })
+    .on(Cart_Route, () => {
+      changePage(Cart());
+    })
+    .on(Home_Route, () => {
+      changePage(home());
+    })
+    .on(Singel_Route,()=>{
+      changePage(singelProduct())
+    })
+    .on(`/${route}`,()=>{
+      changePage(brandProducts(route))
+    })
 
   return router;
 };
