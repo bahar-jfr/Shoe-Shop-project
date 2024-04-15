bb6eea9f29bb32a8dd50702326a28d704a824225
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-04-15 21:20:16 +0330

    feat: uppdate orders list by clicking on counter

--- src/utils/functions/counter/counter.js
+++ src/utils/functions/counter/counter.js
@@ -1,12 +1,27 @@
-export function counter(element, product) {
-    let countNum = document.getElementById(`num${product.id}`);
-    let totalPriceProduct = document.getElementById(`price${product.id}`);
-  
-      if (element.target.id === "plus") {
-        countNum.innerText++;
-      } else if (countNum.innerText != 0 && element.target.id === "mines") {
-        countNum.innerText--;
-      }
-      totalPriceProduct.innerText = `$${product.price * countNum.innerText}.00`;
+import { get } from "../../../api/get/get";
+import { set } from "../../../api/set/set";
 
+export async function counter(element, product) {
+  let countNum = document.getElementById(`num${product.id}`);
+  let totalPriceProduct = document.getElementById(`price${product.id}`);
+
+  if (element.target.id === "plus") {
+    countNum.innerText++;
+  } else if (countNum.innerText != 0 && element.target.id === "mines") {
+    countNum.innerText--;
+  }
+
+  totalPriceProduct.innerText = `$${product.price * countNum.innerText}.00`;
+  product.quantity = countNum.innerText;
+  product.totalPrice = totalPriceProduct.innerText;
+
+  const arrayOfOrders = await get("users").then((res) => {
+    return res.data[0];
+  });
+  if (location.pathname === "/cart") {
+    let findItem = arrayOfOrders.orders.find((item) => item.id == product.id);
+    findItem.quantity = product.quantity;
+    findItem.totalPrice = product.totalPrice;
+    set(arrayOfOrders)
+  }
 }
