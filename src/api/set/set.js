da9858d299879274be2dca2ce658e488d1b40cf4
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-04-17 23:55:03 +0330

    feat: set data on user endpoint

--- src/api/set/set.js
+++ src/api/set/set.js
@@ -2,5 +2,16 @@ import axios from "axios";
 import { BASE_URL } from "../BASEURL";
 
 export function set(item) {
+  console.log(item.cart);
+  axios.patch(`${BASE_URL}/users/1`, item);
+}
+
+export function setFromCart(item) {
+  console.log(item);
+  axios.patch(`${BASE_URL}/users/1`, item);
+}
+
+export function setDeleted(item) {
+  console.log(item);
   axios.patch(`${BASE_URL}/users/1`, item);
 }
