ffc28db68a0b033aadd5bd94fd2fce2bf9963865
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-04-19 11:23:44 +0330

    feat: implement api

--- src/api/get/get.js
+++ src/api/get/get.js
@@ -5,3 +5,8 @@ export async function get(endpoint) {
   const res = await axios.get(`${BASE_URL}/${endpoint}`);
   return res;
 }
+
+export async function search(searchItem = "") {
+  const res = await axios.get(`${BASE_URL}/products?name_like=${searchItem}`);
+  return res;
+}
--- src/api/set/set.js
+++ src/api/set/set.js
@@ -2,16 +2,19 @@ import axios from "axios";
 import { BASE_URL } from "../BASEURL";
 
 export function set(item) {
-  console.log(item.cart);
+ 
   axios.patch(`${BASE_URL}/users/1`, item);
 }
 
 export function setFromCart(item) {
-  console.log(item);
+
   axios.patch(`${BASE_URL}/users/1`, item);
 }
 
 export function setDeleted(item) {
-  console.log(item);
+
+  axios.patch(`${BASE_URL}/users/1`, item);
+}
+export function setWish(item) {
   axios.patch(`${BASE_URL}/users/1`, item);
 }
