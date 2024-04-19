afae99d3aa5b4e6dedf85a7749c59e5dce1b3013
Bahar Jafarimehr <beatricemikaelson23@gmail.com>
2024-04-19 11:27:25 +0330

    feat: implement main page

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+import { Router } from "./src/routes/routes";
 import "./style.css";
+
+if (location.pathname === "/") {
+  Router().navigate("/");
+} else {
+  Router().navigate(location.pathname);
+}
+
+
